0ddd7a8b788861c6cad03abd141fe76d479e715b
jonakazan <jonakazan@gmail.com>
2026-03-09 19:04:55 -0300

    Add files via upload

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -1,29 +1,37 @@
-<?php 
-#ini_set( 'display_errors', 1 );
-#error_reporting( E_ALL );
-$nombre = $_POST['name'];
-$mail = $_POST['email'];
-$mail2 = 'epet3mobera@gmail.com';
-$subject = $_POST['subject'];
-$empresa = $_POST['message'];
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = strip_tags(trim($_POST['name']));
+    $apellido = strip_tags(trim($_POST['surname']));
+    $mail = filter_var(trim($_POST['email']), FILTER_SANITIZE_EMAIL);
+    $subject = strip_tags(trim($_POST['subject']));
+    $mensaje_usuario = strip_tags(trim($_POST['message']));
 
-$header = 'from: '.$mail2."\r\n";
-$header .= "X-Mailer:PHP/".phpversion()."\r\n";
-$header .= "Mime-Version:1.0 \r\n";
-$header .= "content-Type:text/plain";
+    $para = 'jonakazan@gmail.com';
+    $asunto = 'Nuevo mensaje de la Web: ' . $subject;
 
-$message = "Este mensaje fue enviado por: ".$nombre."\r\n";
-$message .= "Su e-mail es: ".$mail."\r\n";
-$message .= "asusnto: ".$subject."\r\n";
-$message .= "mensaje: ".$_POST['message']."\r\n";
-$message .= "enviado el: ".date('d/m/Y',time());
+    // IMPORTANTE: El "From" debe ser una cuenta del servidor o una genérica para evitar spam.
+    // Gmail suele bloquear si el "From" y el "To" son iguales o si el From es el del cliente.
+    $form_email = "no-reply@añosluz.ar";
 
+    $header = "From: Años Luz Web <$form_email>\r\n";
+    $header .= "Reply-To: $mail\r\n";
+    $header .= "MIME-Version: 1.0\r\n";
+    $header .= "Content-Type: text/plain; charset=UTF-8\r\n";
+    $header .= "X-Mailer: PHP/" . phpversion();
 
-$para = 'epet3mobera@gmail.com';
-$asunto = 'mensaje de www.epet3obera.edu.ar';
+    $cuerpo = "Has recibido un nuevo mensaje desde el formulario de contacto:\r\n\r\n";
+    $cuerpo .= "Nombre: " . $nombre . " " . $apellido . "\r\n";
+    $cuerpo .= "E-mail: " . $mail . "\r\n";
+    $cuerpo .= "Asunto: " . $subject . "\r\n\r\n";
+    $cuerpo .= "Mensaje:\r\n" . $mensaje_usuario . "\r\n\r\n";
+    $cuerpo .= "Enviado el: " . date('d/m/Y H:i:s');
 
-if(mail($para,$asunto,utf8_decode($message),$header))
-echo "<script type='text/javascript'>alert('Tu mensaje ha sido enviado correctamente');</script>";
-echo "<script type='text/javascript'>window.location.href='index.html';</script>";
- ?>
- 
+    if (mail($para, $asunto, $cuerpo, $header)) {
+        echo "OK";
+    } else {
+        echo "Error al enviar el mensaje. Verifica la configuración del servidor.";
+    }
+} else {
+    echo "Acceso denegado.";
+}
+?>
